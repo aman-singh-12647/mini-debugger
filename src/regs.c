7e451fe4cac63df08e17784ec0117980c0121835
Gaurav Jain <43726919+gauravjain2@users.noreply.github.com>
2025-12-15 15:14:09 +0530

    Extend register dump to general purpose registers

--- src/regs.c
+++ src/regs.c
@@ -8,7 +8,14 @@ void print_registers(pid_t pid) {
     ptrace(PTRACE_GETREGS, pid, NULL, &regs);
 
     printf("RIP = 0x%llx\n", regs.rip);
+    printf("RAX = 0x%llx\n", regs.rax);
+    printf("RBX = 0x%llx\n", regs.rbx);
+    printf("RCX = 0x%llx\n", regs.rcx);
+    printf("RDX = 0x%llx\n", regs.rdx);
+    printf("RSI = 0x%llx\n", regs.rsi);
+    printf("RDI = 0x%llx\n", regs.rdi);
     printf("RSP = 0x%llx\n", regs.rsp);
     printf("RBP = 0x%llx\n", regs.rbp);
+    printf("EFLAGS = 0x%llx\n", regs.eflags);
 }
 
