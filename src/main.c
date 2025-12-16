e64a4c55c10d7bd6321def4c15c0260b9ce4ce12
Aman Singh <aman.singh2gmail.com>
2025-12-16 18:01:54 +0530

    Implement breakpoint management functions and integrate with main debugger loop

---
+++ src/main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "debugger.h"
+#include "breakpoints.h"
+
+/**
+ * main - Entry point for the mini-debugger application
+ * @argc: Argument count
+ * @argv: Argument vector containing the program to debug
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+  if (argc < 2)
+  {
+    printf("Usage: %s <program>\n", argv[0]);
+    return 1;
+  }
+
+  pid_t pid = start_debugger(argv[1]);
+  struct breakpoint bp = {0};
+
+  char command[64];
+
+  while (1)
+  {
+    printf("dbg> ");
+    fgets(command, sizeof(command), stdin);
+
+    if (strncmp(command, "break", 5) == 0)
+    {
+      void *addr = (void *)strtoul(command + 6, NULL, 16);
+      bp.addr = addr;
+      set_breakpoint(pid, &bp);
+    }
+    else if (strncmp(command, "cont", 4) == 0)
+    {
+      continue_execution(pid);
+      if (bp.enabled)
+      {
+        handle_breakpoint(pid, &bp);
+      }
+    }
+    else if (strncmp(command, "exit", 4) == 0)
+    {
+      break;
+    }
+    else
+    {
+      printf("Unknown command\n");
+    }
+  }
+
+  return 0;
+}
